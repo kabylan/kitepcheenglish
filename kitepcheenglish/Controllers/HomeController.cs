86b51f7cb445ac06c68c99fc10b23f1c9ae2fd8f
Kushtar <eseptehelp@gmail.com>
2020-11-22 12:56:04 +0600

    Добавьте файлы проекта.

---
+++ kitepcheenglish/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using kitepcheenglish.Models;
+
+namespace kitepcheenglish.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("Watch");
+        }
+
+        [Route("/Type")]
+        public IActionResult Type()
+        {
+            return View();
+        }
+
+
+        [Route("/Watch")]
+        public IActionResult Watch()
+        {
+            return View();
+        }
+
+    }
+}
